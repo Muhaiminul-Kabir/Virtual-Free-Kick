77f228592e9a4a75d93bbbf368526d9171d51b1d
Muhaiminul-Kabir <muhaiminulkabir32@gmail.com>
2021-06-15 05:22:35 +0600

    kl

--- main.c
+++ main.c
@@ -11,11 +11,15 @@
 
 #include "raylib.h"
 #include "math.h"
+
 #define RAD  (3.1416/180)
 //global var initalization//start
 
 int screenWidth = 1020;
 int screenHeight = 750;
+
+
+bool bounce = false;
 
 const float g = 9.8f ;
 float  u = 16.93302;
@@ -27,6 +31,7 @@ float x = 0.0f,y ,z;
 
 
 //functions//start
+
 
 
 float projectileDis(float theta, float v0) {
@@ -61,8 +66,16 @@ void projectile(){
     z = tan(c)*x - ((g*x*x)/((2*u*cos(c))*(2*u*cos(c))));
 }
 
+void horzProjecitle(){
+    y = -g*(x/(u*u))/2;
+}
+
+
 
 //end
+
+//
+///
 
 int main(void)
 {
@@ -74,7 +87,7 @@ int main(void)
     InitWindow(screenWidth, screenHeight, "raylib [core] example - input mouse wheel");
     
     Camera3D camera = { 0 };
-    camera.position = (Vector3){ -40,1,0 };  // Camera position
+    camera.position = (Vector3){ -7,1,0 };  // Camera position
     camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
     camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
     camera.fovy = 45.0f;                                // Camera field-of-view Y
@@ -84,40 +97,42 @@ int main(void)
     int boxPositionY = screenHeight/2 - 40;
     int scrollSpeed = 4;            // Scrolling speed in pixels
 
-    SetTargetFPS(30);               // Set our game to run at 60 frames-per-second
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
     
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         // Update
-        UpdateCamera(&camera);   
+        //UpdateCamera(&camera);   
         //----------------------------------------------------------------------------------
         SetExitKey(KEY_L); 
         Vector3 pos = { x, y,z};
         Vector3 centerPos = {0,0,0};
-        Vector3 startPos = {50,0,0};
-        Vector3 endPos = {50,0.9,0};
+        Vector3 startPos = {30,0,0};
+        Vector3 endPos = {30,0.9,0};
         Vector2 size = {50,100};
-        Vector3 startPos2 = {50,0,2};
-        Vector3 endPos2 = {50,0.9,2};
-       
+        Vector3 startPos2 = {30,0,2};
+        Vector3 endPos2 = {30,0.9,2};
+      
         boxPositionY -= (GetMouseWheelMove()*scrollSpeed);
         if(IsKeyDown(KEY_RIGHT)){
-           z++;
+           y -= .1;
         }
         if(IsKeyDown(KEY_UP)){
           x = 0;     
         }
       
         if(IsKeyDown(KEY_LEFT)){
-          x+=1;
+          x+=.1;
         }
         if(IsKeyDown(KEY_K)){
             x= 0;
         }
 
-        
+        if(y<0){
+            bounce = true;
+        }
       
         
         //----------------------------------------------------------------------------------
@@ -134,16 +149,34 @@ int main(void)
                 DrawPlane(centerPos,size, GREEN);
                 DrawLine3D(startPos,endPos, WHITE);
                 DrawLine3D(startPos2,endPos2, WHITE);
-                DrawSphere(pos,0.1f,RED);
+                DrawSphere(pos,.1,RED);
+               
                 // DrawSphere(pos2,1.0f,RED);
-             
+               
+             //   kick();
+                
                 // DrawGrid(10,10);
-                projectile();
+                if(!bounce)
+                    projectile();
         
+                else{
+                    y = 0.09;
+                    
+                    //x++;
+                 if(x>startPos.x){
+                        bounce = false;
+                        x = 0;
+                    }
+            }
         
              //end
              EndMode3D();
              //  DrawText("j", y, 10, 20, MAROON);
+             DrawText(TextFormat("Y %f",y), 10, 10, 20, MAROON);
+             DrawText(TextFormat("X %f",x), 10, 30, 20, MAROON);
+          
+           DrawText(TextFormat("Z %f",z), 10, 50, 20, MAROON);
+            DrawText(TextFormat("targetZ %i", GetMouseX()), 300, 50, 20, MAROON);
           
         EndDrawing();
         //----------------------------------------------------------------------------------
